91525a9969b112e8bf78058d9268a5647f7c405b
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2024-01-10 08:41:58 -0300

    Initial commit

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from '../App';
+
+test('I am your test', () => {
+  const I = "your father";
+  expect(I).toBe("your father");
+});
